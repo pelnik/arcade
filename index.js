24384c7971b8534f9358e5db187e3dcf9a28498a
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-06 16:33:40 -0600

    added ES Lint and NPM to project

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const numberOfColsForLargeScreen = 40;
 const numberOfRowsForLargeScreen = 28;
 
 
+// Box ids start from one
 if (window.screen.width > largeScreenCutoff) {
   for (let i = 0; i < numberOfColsForLargeScreen * numberOfRowsForLargeScreen; i++) {
     const newElement = document.createElement('div');
     newElement.className = 'box';
+    newElement.id = `box-${i + 1}`;
     gameContentElement.appendChild(newElement);
   }
+}
+
+function getBoxNumber(element) {
+  return Number(element.id.slice(4));
 }
