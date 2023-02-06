a66092c639dba1599038b8e8653a676179582344
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-06 17:07:22 -0600

    added early info for snake object

---
+++ Modules/snake.js
@@ -0,0 +1,10 @@
+export default class Snake {
+  constructor(startX, startY) {
+    this.body = [];
+    this.body.push([startX, startY]);
+  }
+
+  drawSnake() {
+    
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+import Snake from '/Modules/snake.js';
+
 const gameContentElement = document.querySelector('#game-content');
 
 const largeScreenCutoff = 768;
 const numberOfColsForLargeScreen = 40;
 const numberOfRowsForLargeScreen = 28;
+const numberOfBoxes = 40 * 28;
 
+let snakeStartXPos = numberOfRowsForLargeScreen;
+let snakeStartYPos = numberOfColsForLargeScreen;
+
+const boxMapToPos = {};
 
 // Box ids start from one
 if (window.screen.width > largeScreenCutoff) {
-  for (let i = 0; i < numberOfColsForLargeScreen * numberOfRowsForLargeScreen; i++) {
+  for (let i = 0; i < numberOfBoxes; i += 1) {
     const newElement = document.createElement('div');
     newElement.className = 'box';
     newElement.id = `box-${i + 1}`;
@@ -15,6 +22,23 @@ if (window.screen.width > largeScreenCutoff) {
   }
 }
 
+// Map boxes to X and Y values
+for (let i = 1; i <= numberOfBoxes; i += 1) {
+  const xyObject = {};
+
+  xyObject.x = i % numberOfColsForLargeScreen;
+  xyObject.y = Math.ceil(i / numberOfRowsForLargeScreen);
+
+  boxMapToPos[`box-${i}`] = xyObject;
+}
+
+console.log(boxMapToPos);
+
+// Need to actually reverse map this somehow
+
 function getBoxNumber(element) {
   return Number(element.id.slice(4));
 }
+
+
+new Snake();
